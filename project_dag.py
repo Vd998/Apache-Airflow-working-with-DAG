a1a73e915a0043d0c1c021fdaf150b5734ac284d
Vd998 <113101094+Vd998@users.noreply.github.com>
2022-09-08 15:41:36 +0530

    Add files via upload
    MY SAMPLE PROJECT ON TOLL DATA

---
+++ project_dag.py
@@ -0,0 +1,79 @@
+# import the libraries
+from datetime import timedelta
+# The DAG object; we'll need this to instantiate a DAG
+from airflow import DAG
+# Operators; we need this to write tasks!
+from airflow.operators.bash_operator import BashOperator
+# This makes scheduling easy
+from airflow.utils.dates import days_ago
+
+
+#defining DAG arguments
+default_args = {
+    'owner': 'Dhairya Vasavada',
+    'start_date': days_ago(0),
+    'email': ['dhairyavasavada@somemail.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+
+# define the DAG
+dag = DAG(
+    dag_id='ETL_TOLL_DATA',
+    default_args=default_args,
+    description='Apache Airflow Assignment',
+    schedule_interval=timedelta(days=1),
+)
+
+
+# define the task unzip
+unzip = BashOperator(
+    task_id='unzip',
+    bash_command='tar xopf tolldata.tar',
+    dag=dag,
+)
+
+
+# define task extract from csv
+extract_from_csv=BashOperator(
+    task_id = 'extract_from_csv',
+    bash_command = 'cut -d"," -f1,2,3,4 vehicle-data.csv > csv_data.csv',
+    dag=dag,
+)
+
+# define task extract from tsv
+extract_from_tsv=BashOperator(
+    task_id = 'extract_from_tsv',
+    bash_command = 'cut -f5,6,7 tollplaza-data.tsv > tsv_data.csv',
+    dag=dag,
+)
+
+#define the task extract_data_from_fixed_width.
+extract_data_from_fixed_width=BashOperator(
+    task_id = 'extract_data_from_fixed_width',
+    bash_command='cut -c 59-68 payment-data.txt > fixed_width_data.csv',
+    dag=dag,
+)
+
+# define the task consolidate data
+consolidate_data=BashOperator(
+    task_id='consolidate_data',
+    bash_command='paste csv_data.csv tsv_data.csv fixed_width_data.csv > extracted_data.csv',
+    dag=dag,
+    )
+
+# define tranfrom and load task to convert lower case charc. to upper case charc.
+transform_and_load=BashOperator(
+    task_id='transfrom_and_load',
+    bash_command='tr "[a-z]" "[A_Z]" < extracted_data.csv > tranformed_data.csv',
+    dag=dag,
+)
+
+
+# task pipeline
+
+
+unzip >> extract_from_csv >> extract_from_tsv >> extract_data_from_fixed_width >> consolidate_data >> transform_and_load
